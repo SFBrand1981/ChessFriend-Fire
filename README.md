54cff54bc7f1238c92cd66de9e5677640257cb61
appletomato <appletomato@users.noreply.github.com>
2019-03-18 18:41:54 +0100

    alpha-version

---
+++ README.md
@@ -0,0 +1,48 @@
+# NW.js-examples
+
+[NW.js][nwjs] features demonstrated in a simple project.
+
+![application screenshot][screen]
+
+This project is part of the article [Building a Cross-Platform Desktop App with NW.js][article] on Sitepoint.
+
+## Requirements
+
+- [Node.js][nodejs]
+
+## Installation
+
+1. Clone or [download][download] this repository
+2. Navigate to the project folder with a CLI
+3. Run the following:
+
+   ```bash
+   $ npm install
+   $ cd src/
+   $ npm install
+   $ cd ..
+   ```
+
+## Development
+
+To run the application directly in your development environment run:
+
+```bash
+$ npm run dev
+```
+
+## Production
+
+To build the application for Windows 32/64 bit, Mac OS X 64 bit and Linux 32/64 bit run the following:
+
+```bash
+$ npm run prod
+```
+
+The output files can be found under `dist/`.
+
+[nwjs]: http://nwjs.io/
+[screen]: https://github.com/sitepoint-editors/nw.js-examples/blob/master/.github/screen.png
+[article]: https://www.sitepoint.com/cross-platform-desktop-app-nw-js
+[nodejs]: https://nodejs.org/en/
+[download]: https://github.com/sitepoint-editors/nw.js-examples/archive/master.zip
---
+++ src/app/importWorker.js
@@ -0,0 +1,91 @@
+// Worker to import large number of PGNs into DB
+var fs = require('fs')
+var path = require('path')
+var cfdt = require(path.join(process.cwd(), '/app/chessfriendDevTools'))
+var BoardHandling = require(path.join(process.cwd(), '/app/boardHandling.js'))
+
+var board = new BoardHandling()
+var nodes = {}
+
+var batch_size = 100
+
+var import_data
+var num_games
+var num_imported
+
+function importBatch() {
+
+    var games = import_data.split(/(\[Event\s.*\])/)
+    var offset = num_imported
+
+    
+    for (var i = 2*offset; i < 2*(offset + batch_size); i += 2) {
+
+	if (i >= 2*num_games) {
+	    return
+	}
+	
+	var game = games[i+1] + games[i+2]
+
+	board.importOneGame(game, function(pgn) {
+	    board.createNodesFromPGN(pgn, nodes, function(n) {
+		
+		num_imported += 1
+		
+		process.send({ importWorker : {importing : true,
+	     				       pgn : pgn,
+	     				       nodes : n,
+	     				       num_imported : num_imported} })
+
+		if (num_imported === num_games) {
+		    process.send({ importWorker : 'imported' })
+		}
+		
+	    })
+	})
+
+    }
+    
+    process.send({ importWorker : {batch_completed : true,
+	     			   num_imported : num_imported} })
+
+	
+}
+
+
+
+process.on('message', (msg) => {
+
+
+    if (msg.importWorker.startImport) {
+	
+	fs.readFile(msg.importWorker.filename, 'utf8', function (err, data) {
+
+	    if (err) {
+		alert("There was an error attempting to read your data.")
+		cfdt.warn(err.message)
+		return
+	    }
+	
+	    function count() {
+		const re = /\[Event\s.*\]/g
+		return ((data || '').match(re) || []).length
+	    }
+	    
+	    num_games = count()
+	    process.send({ importWorker : {num_games : num_games} })
+	    
+	    import_data = data
+	    num_imported = 0
+
+	    importBatch()
+
+	})
+	
+    }
+
+    if (msg.importWorker === 'continueImport') {
+	importBatch()
+    }
+})
+
